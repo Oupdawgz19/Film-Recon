a1aacf13a34e0bffe267f8445d84b5a86fb01158
Lesego Motsoahae <98163443+Oupdawgz19@users.noreply.github.com>
2026-06-05 10:25:30 +0200

    Update products.js

--- phase2/frontend/src/products.js
+++ phase2/frontend/src/products.js
@@ -1,12 +1,16 @@
 
 export default [
-{id:1,name:'FR Coverall',price:899},
-{id:2,name:'Chef Wear Set',price:499},
-{id:3,name:'Security Uniform',price:799},
-{id:4,name:'Reflective Jacket',price:299},
-{id:5,name:'Safety Vest',price:149},
+{id:1,name:'FR Overall',price:1199},
+{id:2,name:'Chef Wear Set',price:1499},
+{id:3,name:'Security Uniform',price:1399},
+{id:4,name:'Reflective Jacket',price:499},
+{id:5,name:'Safety Vest',price:249},
 {id:6,name:'Heavy Duty Boots',price:1199},
-{id:7,name:'Cut Resistant Gloves',price:199},
+{id:7,name:'Cut Resistant Gloves',price:249},
 {id:8,name:'Hard Hat',price:179},
-{id:9,name:'Corporate Welcome Pack',price:499}
+{id:9,name:'Corporate Welcome Pack',price:699}
+{id:10,name:'Hats',price:159}
+{id:11,name:'harness',price:499-1799}
+{id:12,name:'First-Aid Kit',price:499-1599}
+{id:13,name:'Drug test',price:299}
 ];
